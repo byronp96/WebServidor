47ed61f383e06b09434c9c413ef89ac64b49621e
byronp96 <31906283+byronp96@users.noreply.github.com>
2017-09-19 13:06:50 -0600

    Add files via upload

--- php/Ejercicio4.php
+++ php/Ejercicio4.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
     <meta charset="utf-8">
     <title>Ejercicio 4</title>
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/style.css">
-  </head>
-  <body>
+</head>
+
+<body>
     <div class="wrapper">
-<div class="form-signin" >
-  <h2 class="form-signin-heading">Ejercicio 4</h2>
-  <form action="Ejercicio1.php" method="post">
-  <div class="row">
-    <input type="number" class="form-control" name="diametro" placeholder="Diametro" required="" autofocus="" />
-  </div>
-  <div class="row">
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Calcular</button>
-  </div>
-  </form>
-  <div class="row text-center">
-  <?php
+        <div class="form-signin">
+            <h2 class="form-signin-heading">Ejercicio 4</h2>
+            <form action="Ejercicio1.php" method="post">
+                <div class="row">
+                    <input type="number" class="form-control" name="diametro" placeholder="Diametro" required="" autofocus="" />
+                </div>
+                <div class="row">
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">Calcular</button>
+                </div>
+            </form>
+            <div class="row text-center">
+                <?php
   if ($_POST) {
     $diametro = $_POST['diametro'];
     $radio = $diametro/2;
@@ -28,8 +30,12 @@
     echo "Area $area";
   }
   ?>
-</div>
-</div>
-</div>
-  </body>
+            </div>
+            <div class="row text-center">
+                <a href="../index.html">Inicio</a>
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
